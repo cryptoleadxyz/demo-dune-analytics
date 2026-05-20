1b8f8708f0c14f7d38a20fd9ee99952612cf2600
CryptoLead <103460389+cryptoleadxyz@users.noreply.github.com>
2026-05-20 11:15:38 -0600

    Remove comments from main.py
    Removed author and donation comments from main.py

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-# Author: CryptoLead at https://www.cryptolead.xyz
-# Date: 2023-03-19
-# Message: Hey there fellow coders! If you found my code helpful and want to show your support, consider buying me a coffee or two. Your donations help me keep improving the code and creating more awesome stuff for the community. Thanks for your support!
-# Donation: cryptolead.eth or 0xa2c35DA418f52ed89Ba18d51DbA314EB1dc396d0
-
 import os
 import matplotlib.pyplot as plt
 from datetime import date
